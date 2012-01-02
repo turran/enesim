b62d6035da910bcb2157b3cbfef72c6379c91e52
Jorge Zapata <jorgeluis.zapata@gmail.com>
2012-01-02 20:05:19 +0000

    Add a simple geometry shader for doing some research

---
+++ src/lib/renderer/enesim_renderer_rectangle.glsl
@@ -0,0 +1,25 @@
+ENESIM_OPENGL_SHADER(
+uniform float rectangle_width;
+uniform float rectangle_height;
+uniform float rectangle_x;
+uniform float rectangle_y;
+
+void main()
+{
+	//gl_Position = gl_ProjectionMatrix  * gl_Position;
+
+	gl_Position.xy = vec2(rectangle_x, rectangle_y);
+	EmitVertex();
+
+	gl_Position.xy = vec2(rectangle_x + rectangle_width, rectangle_y);
+	EmitVertex();
+
+	gl_Position.xy = vec2(rectangle_x + rectangle_width, rectangle_y + rectangle_height);
+	EmitVertex();
+
+	gl_Position.xy = vec2(rectangle_x, rectangle_y + rectangle_height);
+	EmitVertex();
+
+	EndPrimitive();
+}
+);
