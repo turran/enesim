25c0dbcd776f55436c6a8138237261dae63a6592
Jorge Zapata <jorgeluis.zapata@gmail.com>
2012-06-15 17:52:00 +0000

    Add missing shader

---
+++ enesim/lib/renderer/enesim_renderer_opengl_common_merge.glsl
@@ -0,0 +1,13 @@
+ENESIM_OPENGL_SHADER(
+uniform sampler2D merge_texture_0;
+uniform sampler2D merge_texture_1;
+
+void main()
+{
+	vec4 texel0 = texture2D(merge_texture_0, gl_TexCoord[0]);
+	vec4 texel1 = texture2D(merge_texture_1, gl_TexCoord[0]);
+	gl_FragColor = mix(texel1, texel0, texel0.a);
+	gl_FragColor = texel1.aaaa;
+	//gl_TexCoord[0]; //vec4(1.0, 0.0, 1.0, 1.0);
+}
+)
