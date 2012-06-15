cdb193bdeeccb1fcd5b0a94e25aa29d519b60313
Jorge Zapata <jorgeluis.zapata@gmail.com>
2012-06-15 17:58:17 +0000

    Make it show the final path

--- enesim/lib/renderer/enesim_renderer_opengl_common_merge.glsl
+++ enesim/lib/renderer/enesim_renderer_opengl_common_merge.glsl
@@ -7,7 +7,7 @@ void main()
 	vec4 texel0 = texture2D(merge_texture_0, gl_TexCoord[0]);
 	vec4 texel1 = texture2D(merge_texture_1, gl_TexCoord[0]);
 	gl_FragColor = mix(texel1, texel0, texel0.a);
-	gl_FragColor = texel1.aaaa;
+	//gl_FragColor = texel1.aaaa;
 	//gl_TexCoord[0]; //vec4(1.0, 0.0, 1.0, 1.0);
 }
 )
