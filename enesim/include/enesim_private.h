0c302a395bc6a11004fc0dcd98e0f08cb6ea939d
Vincent Torri <vincent.torri@gmail.com>
2012-07-06 14:47:54 +0000

    define EAPI in enesim_private.h. EAPI needs to be defined on Windows so that symbols are exported. Usually it's done by including Enesim.h, but since the reorganisation of the headers inclusion, one must define EAPI somewhere else.

--- enesim/include/enesim_private.h
+++ enesim/include/enesim_private.h
@@ -112,4 +112,30 @@ extern int enesim_log_dom_global;
 
 #define ENESIM_RENDERER_PROXY_MAGIC 0xe7e51463
 
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef ENESIM_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! ENESIM_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif
+
 #endif
