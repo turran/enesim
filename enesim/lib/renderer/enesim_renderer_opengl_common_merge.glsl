c57286ea00befe4257b474cb0d955febba8113eb
Jorge Zapata <jorgeluis.zapata@gmail.com>
2012-07-14 16:43:42 +0000

    make the shader work with mesa

--- enesim/lib/renderer/enesim_renderer_opengl_common_merge.glsl
+++ enesim/lib/renderer/enesim_renderer_opengl_common_merge.glsl
@@ -4,8 +4,8 @@ uniform sampler2D merge_texture_1;
 
 void main()
 {
-	vec4 texel0 = texture2D(merge_texture_0, gl_TexCoord[0]);
-	vec4 texel1 = texture2D(merge_texture_1, gl_TexCoord[0]);
+	vec4 texel0 = texture2D(merge_texture_0, gl_TexCoord[0].xy);
+	vec4 texel1 = texture2D(merge_texture_1, gl_TexCoord[0].xy);
 	gl_FragColor = mix(texel1, texel0, texel0.a);
 	//gl_FragColor = texel1.aaaa;
 	//gl_TexCoord[0]; //vec4(1.0, 0.0, 1.0, 1.0);
