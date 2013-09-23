c4244de08de91ddaa31b0aad030636bef0aed8e4
Jorge Zapata <jorgeluis.zapata@gmail.com>
2013-09-24 00:40:03 +0200

    Remove old m4 files and use new common

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,13 @@
 rm -rf autom4te.cache
 rm -f aclocal.m4 ltmain.sh
 
+# Make sure we have common
+if [ ! -d common/m4 ]; then
+	echo "+ Setting up common submodule"
+	git submodule init
+fi
+git submodule update
+
 autoreconf -f -i -v --warnings=all || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
