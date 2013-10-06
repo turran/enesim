6d2a6fa3b330b45c04df033afab6caa78677c0cb
Jorge Zapata <jorgeluis.zapata@gmail.com>
2013-10-07 00:19:22 +0200

    Improve the blur example

--- src/examples/enesim_renderer_blur01.c
+++ src/examples/enesim_renderer_blur01.c
@@ -12,11 +12,13 @@ static Enesim_Renderer * enesim_renderer_blur01(void)
 	r1 = enesim_renderer_circle_new();
 	enesim_renderer_circle_center_set(r1, 128, 128);
 	enesim_renderer_circle_radius_set(r1, 64);
+	enesim_renderer_shape_stroke_weight_set(r1, 5);
+	enesim_renderer_shape_stroke_color_set(r1, 0xff0000ff);
 	enesim_renderer_shape_fill_color_set(r1, 0xffff0000);
-	enesim_renderer_shape_draw_mode_set(r1, ENESIM_RENDERER_SHAPE_DRAW_MODE_FILL);
+	enesim_renderer_shape_draw_mode_set(r1, ENESIM_RENDERER_SHAPE_DRAW_MODE_STROKE_FILL);
 
 	r2 = enesim_renderer_blur_new();
-	enesim_renderer_origin_set(r2, 15, 15);
+	enesim_renderer_origin_set(r2, 68, 68);
 	enesim_renderer_blur_source_renderer_set(r2, r1);
 	enesim_renderer_blur_radius_x_set(r2, 4.5);
 	enesim_renderer_blur_radius_y_set(r2, 4.5);
