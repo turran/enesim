01902021570cc213f97b4a0e224800250f0cb6e5
Jorge Zapata <jorgeluis.zapata@gmail.com>
2013-10-10 21:16:47 +0200

    Add the text example image and update the others
    Use a clearer checker for the background

--- src/examples/enesim_renderer_example.h
+++ src/examples/enesim_renderer_example.h
@@ -78,10 +78,10 @@ static void run(Enesim_Renderer *r, const char *file, int argc, char **argv)
 	s = enesim_surface_new(ENESIM_FORMAT_ARGB8888,
 			256, 256);
 	c = enesim_renderer_checker_new();
-	enesim_renderer_checker_odd_color_set(c, 0xff444444);
-	enesim_renderer_checker_even_color_set(c, 0xff222222);
-	enesim_renderer_checker_width_set(c, 20);
-	enesim_renderer_checker_height_set(c, 20);
+	enesim_renderer_checker_odd_color_set(c, 0xffcccccc);
+	enesim_renderer_checker_even_color_set(c, 0xffaaaaaa);
+	enesim_renderer_checker_width_set(c, 10);
+	enesim_renderer_checker_height_set(c, 10);
 	/* first draw the background */	
 	if (!enesim_renderer_draw(c, s, ENESIM_ROP_FILL, NULL, 0, 0,
 			&error))
