ac91c6d89cfc126c87f400c6fe90499d5d5a7dd7
Drone Build <support@drone.io>
2013-10-11 19:22:58 +0000

    Updating the docs

--- docs/search/all_78.js
+++ docs/search/all_78.js
@@ -1,10 +1,10 @@
 var searchData=
 [
   ['x',['x',['../structEnesim__Path__Command__Move__To.html#a0342df879eae7c49d6bf73eecb44f8c1',1,'Enesim_Path_Command_Move_To::x()'],['../structEnesim__Path__Command__Line__To.html#a21d91022d8673aca723809c31230ba19',1,'Enesim_Path_Command_Line_To::x()'],['../structEnesim__Path__Command__Squadratic__To.html#ac16be681ae1afffdee332aa296acce33',1,'Enesim_Path_Command_Squadratic_To::x()'],['../structEnesim__Path__Command__Quadratic__To.html#af79e55271d4a595a19e34a5f417b1567',1,'Enesim_Path_Command_Quadratic_To::x()'],['../structEnesim__Path__Command__Cubic__To.html#a75e84c332a5a2b27d2fae3ad80870c88',1,'Enesim_Path_Command_Cubic_To::x()'],['../structEnesim__Path__Command__Scubic__To.html#af45745ffa8c19385200fc79aad1e1ecb',1,'Enesim_Path_Command_Scubic_To::x()'],['../structEnesim__Path__Command__Arc__To.html#abcf34973ddceeb080deb92a6d7556da5',1,'Enesim_Path_Command_Arc_To::x()'],['../structEnesim__Rectangle.html#ae98a0f8df92167af40ed1cc53292c4fa',1,'Enesim_Rectangle::x()'],['../structEnesim__Renderer__Figure__Vertex.html#a0feaebc0384e27dd91d4bb425c27ef33',1,'Enesim_Renderer_Figure_Vertex::x()']]],
-  ['x0',['x0',['../structEnesim__Quad.html#adf8e3e9db62f45d761b7817b8a2954e8',1,'Enesim_Quad']]],
-  ['x1',['x1',['../structEnesim__Quad.html#a9581afae6100941b1bdb74e58989791d',1,'Enesim_Quad']]],
-  ['x2',['x2',['../structEnesim__Quad.html#a64d688f8ee9882b891c39235348af3c1',1,'Enesim_Quad']]],
-  ['x3',['x3',['../structEnesim__Quad.html#a1caa12956c96cab6024dc2bf33660361',1,'Enesim_Quad']]],
+  ['x0',['x0',['../group__Enesim__Quad__Group.html#gadf8e3e9db62f45d761b7817b8a2954e8',1,'Enesim_Quad']]],
+  ['x1',['x1',['../group__Enesim__Quad__Group.html#ga9581afae6100941b1bdb74e58989791d',1,'Enesim_Quad']]],
+  ['x2',['x2',['../group__Enesim__Quad__Group.html#ga64d688f8ee9882b891c39235348af3c1',1,'Enesim_Quad']]],
+  ['x3',['x3',['../group__Enesim__Quad__Group.html#ga1caa12956c96cab6024dc2bf33660361',1,'Enesim_Quad']]],
   ['xrgb8888',['xrgb8888',['../unionEnesim__Buffer__Sw__Data.html#a22b4c96c7cb980c5fd677ed339685742',1,'Enesim_Buffer_Sw_Data']]],
   ['xx',['xx',['../structEnesim__Matrix.html#aa61b22a29225e9d7c2ee916e3f1e2827',1,'Enesim_Matrix::xx()'],['../structEnesim__F16p16__Matrix.html#add7037a258d0b3d642645f8f78d2a1fa',1,'Enesim_F16p16_Matrix::xx()']]],
   ['xy',['xy',['../structEnesim__Matrix.html#a0fae54e9f07895031745082991723459',1,'Enesim_Matrix::xy()'],['../structEnesim__F16p16__Matrix.html#af2de9084a439820ded2b7fc0cbfc662e',1,'Enesim_F16p16_Matrix::xy()']]],
