34dc603121bf242679f4ca99f5a09020dca41576
Vincent Torri <vincent.torri@gmail.com>
2013-10-12 08:44:47 +0200

    fix end of documentation

--- src/lib/enesim_quad.h
+++ src/lib/enesim_quad.h
@@ -51,4 +51,8 @@ EAPI void enesim_quad_coords_get(const Enesim_Quad *q, double *x1, double *y1,
 		double *x2, double *y2, double *x3, double *y3, double *x4,
 		double *y4);
 
+/**
+ * @}
+ */
+
 #endif
