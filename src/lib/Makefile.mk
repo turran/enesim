db08fe7dbbcbeedb56eddd62746397673ec7278a
Jorge Zapata <jorgeluis.zapata@gmail.com>
2013-12-02 20:51:43 +0100

    Be sure to include the curve headers

--- src/lib/Makefile.mk
+++ src/lib/Makefile.mk
@@ -81,6 +81,7 @@ src_lib_libenesim_la_CPPFLAGS = \
 -I$(top_srcdir)/src/lib \
 -I$(top_srcdir)/src/lib/renderer \
 -I$(top_srcdir)/src/lib/object \
+-I$(top_srcdir)/src/lib/curve \
 -I$(top_srcdir)/src/lib/path \
 -I$(top_srcdir)/src/lib/util \
 -I$(top_srcdir)/src/lib/argb \
