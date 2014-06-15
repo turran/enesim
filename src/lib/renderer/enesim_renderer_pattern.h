99c5a258575b1831b783111f893d835fe5f9d679
Jorge Zapata <jorgelius.zapata@gmail.com>
2014-06-15 13:02:34 +0200

    Fix the name of the exported function

--- src/lib/renderer/enesim_renderer_pattern.h
+++ src/lib/renderer/enesim_renderer_pattern.h
@@ -33,7 +33,7 @@ EAPI Enesim_Renderer * enesim_renderer_pattern_new(void);
 EAPI void enesim_renderer_pattern_source_renderer_set(Enesim_Renderer *r, Enesim_Renderer *source);
 EAPI Enesim_Renderer * enesim_renderer_pattern_source_renderer_get(Enesim_Renderer *r);
 EAPI void enesim_surface_pattern_source_surface_set(Enesim_Renderer *r, Enesim_Surface *source);
-EAPI Enesim_Surface * enesim_surface_pattern_source_surface_get(Enesim_Renderer *r);
+EAPI Enesim_Surface * enesim_renderer_pattern_source_surface_get(Enesim_Renderer *r);
 EAPI void enesim_renderer_pattern_repeat_mode_set(Enesim_Renderer *r, Enesim_Repeat_Mode mode);
 EAPI Enesim_Repeat_Mode enesim_renderer_pattern_repeat_mode_get(Enesim_Renderer *r);
 
