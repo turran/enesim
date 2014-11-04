5f4719f0e6842323b07dded873d095ca4f3bd29b
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-11-04 19:39:34 +0100

    Add a parenthesis
    This will allow things like if (foo > MAX(bar1, bar2))

--- src/lib/enesim_private.h
+++ src/lib/enesim_private.h
@@ -59,11 +59,11 @@
 #endif
 
 #ifndef MIN
-#define MIN(a,b) ((a) < (b)) ? (a) : (b)
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
 #endif
 
 #ifndef MAX
-#define MAX(a,b) ((a) > (b)) ? (a) : (b)
+#define MAX(a,b) (((a) > (b)) ? (a) : (b))
 #endif
 
 #include <math.h>
