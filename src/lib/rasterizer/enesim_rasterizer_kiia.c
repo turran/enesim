e0d49a72317b929bc21083e8492254b8ad72ddb0
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-11-17 03:14:08 +0100

    Start drawing something

--- src/lib/rasterizer/enesim_rasterizer_kiia.c
+++ src/lib/rasterizer/enesim_rasterizer_kiia.c
@@ -62,8 +62,13 @@ typedef struct _Enesim_Rasterizer_Kiia_Worker
 /* Its own definition of an edge */
 typedef struct _Enesim_Rasterizer_Kiia_Edge
 {
-	Eina_F16p16 xx0, yy0, xx1, yy1;
+	/* The top/bottom coordinates rounded to increment of 1/num samples */
+	Eina_F16p16 yy0, yy1;
+	/* The left/right coordinates in fixed point */
+	Eina_F16p16 xx0, xx1;
+	/* The x coordinate at yy0 */
 	Eina_F16p16 mx;
+	/* The increment on x when y increments 1/num samples */
 	Eina_F16p16 slope;
 	int sgn;
 } Enesim_Rasterizer_Kiia_Edge;
@@ -72,6 +77,12 @@ typedef struct _Enesim_Rasterizer_Kiia
 {
 	Enesim_Rasterizer parent;
 	/* private */
+	/* The let most coordinate */
+	Eina_F16p16 lx;
+	/* The number of samples (8, 16, 32) */
+	int nsamples;
+	/* the increment on the y direction (i.e 1/num samples) */
+	Eina_F16p16 inc;
 	/* One worker per cpu */
 	Enesim_Rasterizer_Kiia_Worker *workers;
 	int nworkers;
@@ -88,6 +99,7 @@ static Eina_Bool _kiia_edge_setup(Enesim_Rasterizer_Kiia_Edge *thiz,
 {
 	double x0, y0, x1, y1;
 	double x01, y01;
+	double start;
 	double slope;
 	double mx;
 
@@ -96,48 +108,42 @@ static Eina_Bool _kiia_edge_setup(Enesim_Rasterizer_Kiia_Edge *thiz,
 	x1 = p1->x;
 	y1 = p1->y;
 
-	printf("y = %g %g\n", y0, ((int)(y0 * 4.0))/4.0);
+	/* TODO get the sign */
 
-	thiz->xx0 = eina_f16p16_double_from(x0);
-	thiz->yy0 = eina_f16p16_double_from(y0);
-	thiz->xx1 = eina_f16p16_double_from(x1);
-	thiz->yy1 = eina_f16p16_double_from(y1);
+	/* clamp to subpixel */
+	/* going down, swap the y */
+	if (y0 > y1)
+	{
+		double tmp;
+
+		tmp = y0;
+		y0 = y1;
+		y1 = tmp;
+
+		tmp = x0;
+		x0 = x1;
+		x1 = tmp;
+	}
 	/* get the slope */
 	x01 = x1 - x0;
 	y01 = y1 - y0;
+	/* for horizontal edges we just skip */
+	if (!y01) return EINA_FALSE;
+
 	slope = x01 / y01;
-	thiz->slope = eina_f16p16_double_from(slope);
-	/* get the sign */
-	if ((thiz->yy0 == thiz->yy1) || (thiz->xx0 == thiz->xx1))
-		thiz->sgn = 0;
-	else
-	{
-		thiz->sgn = 1;
-		if (thiz->yy1 > thiz->yy0)
-		{
-			if (thiz->xx1 < thiz->xx0)
-				thiz->sgn = -1;
-		}
-		else
-		{
-			 if (thiz->xx1 > thiz->xx0)
-				thiz->sgn = -1;
-		}
-	}
 
-	if (thiz->xx0 > thiz->xx1)
-	{
-		Eina_F16p16 xx0 = thiz->xx0;
-		thiz->xx0 = thiz->xx1;
-		thiz->xx1 = xx0;
-	}
+	/* get the sampled Y inside the line */
+	start = (ceil(y0 * 32.0) / 32.0);
+	y1 = (floor(y1 * 32.0) / 32.0);
+	mx = x0 + (slope * (start - y0));
 
-	if (thiz->yy0 > thiz->yy1)
-	{
-		Eina_F16p16 yy0 = thiz->yy0;
-		thiz->yy0 = thiz->yy1;
-		thiz->yy1 = yy0;
-	}
+	thiz->yy0 = eina_f16p16_double_from(start);
+	thiz->yy1 = eina_f16p16_double_from(y1);
+	thiz->xx0 = eina_f16p16_double_from(x0);
+	thiz->xx1 = eina_f16p16_double_from(x1);
+	thiz->slope = eina_f16p16_double_from(slope/32.0);
+	thiz->mx = eina_f16p16_double_from(mx);
+
 	return EINA_TRUE;
 }
 
@@ -148,7 +154,7 @@ static Eina_Bool _kiia_edges_setup(Enesim_Renderer *r)
 	Enesim_Polygon *p;
 	const Enesim_Figure *f;
 	Eina_List *l1;
-	int n;
+	int n = 0;
 
 	thiz = ENESIM_RASTERIZER_KIIA(r);
 	rr = ENESIM_RASTERIZER(r);
@@ -173,11 +179,14 @@ static Eina_Bool _kiia_edges_setup(Enesim_Renderer *r)
 		{
 			Enesim_Rasterizer_Kiia_Edge *e = &thiz->edges[n];
 
-			_kiia_edge_setup(e, pp, pt);
-			pp = pt;
+			if (!_kiia_edge_setup(e, pp, pt))
+				goto next;
 			n++;
+next:
+			pp = pt;
 		}
 	}
+	thiz->nedges = n;
 	return EINA_TRUE;
 }
 
@@ -186,15 +195,58 @@ static void _kiia_span(Enesim_Renderer *r,
 {
 	Enesim_Rasterizer_Kiia *thiz;
 	Enesim_Rasterizer_Kiia_Worker *w;
+	Eina_F16p16 yy0, yy1;
+	Eina_F16p16 ll = INT_MAX, rr = -INT_MAX;
 	uint32_t *dst = ddata;
-	uint32_t *buf;
+	int i;
 
 	thiz = ENESIM_RASTERIZER_KIIA(r);
 	w = &thiz->workers[y % thiz->nworkers];
 
-	//printf("y = %d thread %d old y = %d\n", y, y % thiz->nworkers, w->y);
-	/* iterate over the subpixel y and set the mask value */
+	yy0 = eina_f16p16_int_from(y);
+	yy1 = eina_f16p16_int_from(y + 1);
+	/* intersect with each edge */
+	for (i = 0; i < thiz->nedges; i++)
+	{
+		Enesim_Rasterizer_Kiia_Edge *edge = &thiz->edges[i];
+		Eina_F16p16 yyy0, yyy1;
+		Eina_F16p16 inc;
+		Eina_F16p16 cx;
+		int j;
+
+		/* outside the edge */
+		if (yy0 < edge->yy0 || yy0 > edge->yy1)
+			continue;
+		/* make sure not overflow */
+		yyy1 = yy1;
+		if (yyy1 > edge->yy1)
+			yyy1 = edge->yy1;
+
+		/* keep track of the start, end of edges */
+		if (edge->xx0 < ll)
+			ll = edge->xx0;
+		if (edge->xx1 > rr)
+			rr = edge->xx1;
+
+		inc = eina_f16p16_mul(yy0 - edge->yy0, eina_f16p16_int_from(32.0));
+		cx = edge->mx + eina_f16p16_mul(inc, edge->slope);
+		/* finally sample */
+		for (yyy0 = yy0; yyy0 < yyy1; yyy0 += eina_f16p16_double_from(1/32.0))
+		{
+			int mx;
+
+			mx = eina_f16p16_int_to(cx - thiz->lx);
+			//w->mask[mx] ^= 0xffffffff;
+			w->mask[mx] = 0xffff0000;
+			cx += edge->slope;
+		}
+	}
+	//printf("y = %d ll %d rr %d\n", y, eina_f16p16_int_to(ll), eina_f16p16_int_to(rr));
+	/* memset [x ... left] [right ... x + len] */
+	memcpy(dst, w->mask + (x - eina_f16p16_int_to(thiz->lx)), sizeof(uint32_t) * len);
+	memset(w->mask + (x - eina_f16p16_int_to(thiz->lx)), 0, sizeof(uint32_t) * len);
 	/* iterate over the mask and fill */ 
+	/* also clear the mask in the process */
 	/* update the latest y coordinate of the worker */
 	w->y = y;
 }
@@ -233,10 +285,13 @@ static Eina_Bool _kiia_sw_setup(Enesim_Renderer *r,
 	int i;
 	int y;
 
-	/* create the edges */
-	if (!_kiia_edges_setup(r))
-		return EINA_FALSE;
 
+	if (enesim_rasterizer_has_changed(r))
+	{
+		/* create the edges */
+		if (!_kiia_edges_setup(r))
+			return EINA_FALSE;
+	}
 	rr = ENESIM_RASTERIZER(r);
 	if (!enesim_figure_bounds(rr->figure, &lx, &ty, &rx, &by))
 		return EINA_FALSE;
@@ -246,12 +301,13 @@ static Eina_Bool _kiia_sw_setup(Enesim_Renderer *r,
 	/* set the y coordinate with the topmost value */
 	y = ceil(ty);
 	/* the length of the mask buffer */
-	len = ceil(rx - lx) + 1;
+	len = ceil(rx - lx);
+	thiz->lx = eina_f16p16_int_from(floor(lx));
 	/* setup the workers */
 	for (i = 0; i < thiz->nworkers; i++)
 	{
 		thiz->workers[i].y = y;
-		thiz->workers[i].mask = malloc(sizeof(uint32_t) * len * 32);
+		thiz->workers[i].mask = calloc(len + 1, sizeof(uint32_t));
 	}
 	*draw = _kiia_span;
 	return EINA_TRUE;
