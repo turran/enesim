b5d910a6a73b3183866fc3406cb8c7e11d2933eb
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-03-13 21:44:23 +0100

    Fix the .ender rule

--- data/Makefile.mk
+++ data/Makefile.mk
@@ -7,7 +7,7 @@ descriptions_DATA = data/enesim.ender
 
 ender: doxygen
 	@xsltproc --param lib "'enesim'" --param version 0 --param case "'underscore'" @ENDER_TOOLS_DIR@/fromdoxygen.xslt $(top_builddir)/doc/xml/Enesim_8h.xml > out.xml
-	@xmllint -format out.xml > $(top_srcdir)/data/$@
+	@xmllint -format out.xml > $(top_srcdir)/data/enesim.ender
 	@rm out.xml
 
 endif
