cc02305f50e40f24b020ba8e3ef80c4b9f7fddb5
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-03-13 19:09:44 +0100

    Make enesim generate the ender file itself

---
+++ data/Makefile.mk
@@ -0,0 +1,13 @@
+EXTRA_DIST += data/enesim.ender
+
+if ENS_BUILD_ENDER
+
+descriptionsdir = @ENDER_DESCRIPTIONS_DIR@
+descriptions_DATA = data/enesim.ender
+
+ender: doxygen
+	@xsltproc --param lib "'enesim'" --param version 0 --param case "'underscore'" @ENDER_TOOLS_DIR@/fromdoxygen.xslt $(top_builddir)/doc/xml/Enesim_8h.xml > out.xml
+	@xmllint -format out.xml > $(top_srcdir)/data/$@
+	@rm out.xml
+
+endif
