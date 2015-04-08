60eb503e525d2093887833ddf64b9fc74999ec32
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-04-08 23:21:22 +0200

    Be able to iterate over log simply

---
+++ src/lib/enesim_log_private.h
@@ -0,0 +1,29 @@
+/* ENESIM - Drawing Library
+ * Copyright (C) 2007-2013 Jorge Luis Zapata
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef ENESIM_LOG_PRIVATE_H_
+#define ENESIM_LOG_PRIVATE_H_
+
+struct _Enesim_Log
+{
+	int ref;
+	Eina_List *trace;
+};
+
+#define ENESIM_LOG_FOREACH(log, l, data) EINA_LIST_FOREACH(log->trace, l, data)
+
+#endif
