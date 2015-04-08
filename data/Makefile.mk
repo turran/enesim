8346efa8bfae40215fd8c61dcb596954e56f5c32
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-04-08 23:18:59 +0200

    Generate the merged ender file

--- data/Makefile.mk
+++ data/Makefile.mk
@@ -7,6 +7,8 @@ descriptions_DATA = data/enesim.ender
 
 ender: doxygen
 	@xsltproc --param lib "'enesim'" --param version 0 --param case "'underscore'" @ENDER_TOOLS_DIR@/fromdoxygen.xslt $(top_builddir)/doc/xml/Enesim_8h.xml > out.xml
+	@xmllint -format out.xml > $(top_srcdir)/data/enesim-main.ender
+	@xsltproc --param tomerge "'$(top_srcdir)/data/enesim-callbacks.ender'" $(top_srcdir)/data/merge.xslt $(top_srcdir)/data/enesim-main.ender > out.xml
 	@xmllint -format out.xml > $(top_srcdir)/data/enesim.ender
 	@rm out.xml
 
